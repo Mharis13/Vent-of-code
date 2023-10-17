fed18ab162cabba89409db8f307550e31f0529cc
Mario <mario12ban@gmail.com>
2023-10-17 22:13:50 +0200

    Advent of Code Day1 part1

---
+++ AdventofCodeDay1.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+    public class AdventofCodeDay1{
+    public static void main(String[] args) {
+        ReadFile();
+    }
+
+    private static void ReadFile() {
+         File inputFile =  new File("Input.txt");
+         int total = 0, maxCalorie = 0;
+
+         String line = "";
+         
+         try{
+        Scanner readScanner = new Scanner(inputFile);
+
+        while((readScanner).hasNextLine()){
+            line = readScanner.nextLine();
+                
+            
+            if (line.isBlank()) {
+                total = 0;
+            }
+
+            if (!line.isBlank()) total += Integer.valueOf(line);
+
+            if(total > maxCalorie) {
+            maxCalorie = total;
+            }
+            
+        }
+
+        readScanner.close();
+
+        System.out.println(maxCalorie);
+
+        }
+        catch(FileNotFoundException exception){
+        System.out.println("Something is wrong.");
+    }
+    }
+
+}
+
